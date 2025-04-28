e701e0e4e38a9832c6caa7d214ef7f7fb3aef7fe
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-27 21:43:42 -0500

    refactor: simplify user component inputs and update template bindings

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,17 +15,15 @@ import { Component, computed, EventEmitter, Input, Output } from '@angular/core'
 })
 export class UserComponent {
   // this will mark this property as 'setable' from outside the component
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  @Input({required: true}) id!: string;
+  @Input({required: true}) user!: User;
   // EventEmitter; object that will allow is to emit custom values through the output decorator.
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
   // we use the select property to emit a new value with event emitter
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
