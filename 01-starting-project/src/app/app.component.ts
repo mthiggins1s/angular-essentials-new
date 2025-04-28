982348288b2f715d56e37140135d260d2ab5964e
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-27 21:58:35 -0500

    feat: implement tasks component with dynamic user task display and styling

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'ul';
+  selectedUserId?: string; // ? added to tell TypeScript that we know it will be set.
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
